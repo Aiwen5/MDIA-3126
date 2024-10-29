bd478f888654704dedffb7cabbb44fdcf62fa2f3
Evan Schatz <eeschatz@icloud.com>
2024-10-29 15:23:44 -0700

    in class api call

--- midterm-prep1/app/page.js
+++ midterm-prep1/app/page.js
@@ -1,6 +1,5 @@
 "use client"
 
-import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 
 
@@ -16,10 +15,58 @@ import { useState } from "react";
 //  - error handling if the data doesn't come back
 
 export default function Home() {
+  // if useState !null,, probably fetching or loading data, or have data
+  // uf useState === data, we can display our data
+  const [astronomyData, setAstronomyData] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  async function fetchAstronomyData() {
+    setLoading(true);
+    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5");
+
+    const data = await response.json();
+
+    setAstronomyData(data);
+    setLoading(false);
+  }
+
+  const DisplayAstronomyData = () => {
+    if (loading) return <p>Loading...</p>;
+
+    if (astronomyData) {
+      const dataThatIsFormattedForDisplay = astronomyData.map((entry, i) => (
+        <article key={i}>
+          <img src={entry.url} alt={entry.title} style={{ maxWidth: "100%" }} />
+          <h3>{entry.title}</h3>
+          <p>{entry.explanation}</p>
+        </article>
+      ));
+
+      return <section>{dataThatIsFormattedForDisplay}</section>;
+    }
+
+    return (
+      <div>
+        <p>Empty, no data fetched!</p>
+      </div>
+    );
+  };
+
+
+  const Header = () => {
+    // build the UI that grabs data
+    return (
+    <header>Welcome to my MIDTERM PREP</header>
+    );
+  }
+
   return (
     <>
-      <div className={styles.container}>
-        <h1>Midterm Prep</h1>
+      <div>
+        <br />
+        <Header />
+        <button className="border-2" onClick={fetchAstronomyData}>Fetch Stuff</button>    
+        <DisplayAstronomyData />    
       </div>
     </>
   );
